79de11fe9addab52899ae9c65dfbdf078cf8150e
Poojith23 <114901320+Poojith23@users.noreply.github.com>
2022-12-23 23:05:14 +0530

    Add files via upload

---
+++ Python programs on Numeric Functions.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Python code to demonstrate the working of
+# factorial()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = 5
+ 
+# returning the factorial of 5
+print("The factorial of 5 is : ", end="")
+print(math.factorial(a))
+
+
+# In[2]:
+
+
+# Python code to demonstrate the working of
+# gcd()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = 15
+b = 5
+ 
+# returning the gcd of 15 and 5
+print ("The gcd of 5 and 15 is : ", end="")
+print (math.gcd(b, a))
+
+
+# In[3]:
+
+
+# Python code to demonstrate the working of
+# fabs()
+ 
+# importing "math" for mathematical operations
+import math
+ 
+a = -10
+ 
+# returning the absolute value.
+print ("The absolute value of -10 is : ", end="")
+print (math.fabs(a))
+
+
+# In[ ]:
+
+
+
+
